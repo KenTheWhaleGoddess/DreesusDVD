13353a738c53055ffff4dbd3222aad9d8b57fc22
Ken Preiser <kenpreiser@gmail.com>
2023-02-13 20:39:18 -0800

    Add files via upload

--- dvd-webpage/deployed-addresses.js
+++ dvd-webpage/deployed-addresses.js
@@ -1,1 +1,3 @@
-export const contractAddress = "0x0ff1c3c879e0004b2cdb52c432c8c42305a67f12";
+export const originalAddress = "0xe692582fb12C3e85B3b2E29a94a2F3beFbCEC78B";
+export const burnToRedeemAddress = "0x8a8D9657d86d45c72Ded7f3Dc5405c7cF760E139";
+export const burnAddress = "0x000000000000000000000000000000000000dEaD";
